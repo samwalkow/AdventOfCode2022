ac8aac69def0ba7992ae3ffc18bad8152cd3b8df
Sam Walkow <samwalkow@gmail.com>
2022-12-13 11:17:13 -0500

    Day 6 Part 2

--- Day 6/Day6Solution.py
+++ Day 6/Day6Solution.py
@@ -2,20 +2,28 @@ file = open("./Day 6/Day6Input.txt")
 #file = open("./Day 6/TestInputDay6.txt")
 input = file.read()
 
-four_numbers = list()
 
-for index, char in enumerate(input):
-    print(char)
-    if len(four_numbers) < 4:
-        four_numbers.append(char)
-        print(four_numbers)
-    if len(four_numbers) == 4:
-        unique = len(set(four_numbers)) == len(four_numbers)
-        if unique:
-            print("all unique", four_numbers, index+1)
-            break
-        else:
-            four_numbers = four_numbers[1:]
-            four_numbers
+def find_signals(number_of_characters: int) -> int:
+    """Function to find the unique combination of characters that indicate the start of signal or messge
 
+    Args:
+        number_of_characters (int): number of chacaters that indicate the beginning of a signal or message
+
+    Returns:
+        int: the number of characters that come before a signal or message
+    """
+    number_list = list()
+    for index, char in enumerate(input):
+        if len(number_list) < number_of_characters:
+            number_list.append(char)
+        if len(number_list) == number_of_characters:
+            unique = len(set(number_list)) == len(number_list)
+            if unique:
+                return index+1
+            else:
+                number_list = number_list[1:]
+                number_list
+
+print("Part 1:", find_signals(4))
+print("Part 2:", find_signals(14))
         
